aed6eea0ab8a1b74da0be3e3caff3abdeb4cd6d2
Can Temizyurek <can@temizyurek.com>
2026-01-20 11:34:29 -0500

    feat: export utils module in index file

---
+++ .changeset/export-utils.md
@@ -0,0 +1,5 @@
+---
+"ai-sdk-graph": patch
+---
+
+Export utils module from the main package entry point
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './graph'
 export * from './storage'
 export * from './types'
+export * from './utils'
